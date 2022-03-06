f230e8a7e1749928c92a32c77c56203e8107b388
Adamozo <naworsk.adam@gmail.com>
2022-03-06 11:46:19 +0100

    switched main database to postgres

--- hospitations_po/server/database.py
+++ hospitations_po/server/database.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL: str = "postgresql://postgres:postgres@127.0.0.1/pio"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL: str = "postgresql://postgres:postgres@127.0.0.1/po"
+#SQLALCHEMY_DATABASE_URL: str = "sqlite:///./sql_app.db"
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL,
